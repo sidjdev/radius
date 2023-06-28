f540d310c3d2e002535ae7c20c2cc26a10442436
Sidharth J Dev <sid.j.dev@gmail.com>
2023-06-28 13:18:39 +0530

    Initial Commit

---
+++ RadiusAgent/RadiusAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiusAgentApp.swift
+//  RadiusAgent
+//
+//  Created by Sidharth J Dev on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RadiusAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
